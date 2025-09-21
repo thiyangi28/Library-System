196ae02d88e28a2e46d2c306b6bc24dbf3cd0196
thiyangi28 <thiyangip24@gmail.com>
2025-09-21 09:58:13 +0530

    Initial commit

---
+++ src/main/java/edu/icet/Main.java
@@ -0,0 +1,13 @@
+package edu.icet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        SpringApplication.run(Main.class);
+
+
+    }
+}
